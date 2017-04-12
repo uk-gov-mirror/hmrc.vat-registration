ab7c0741ee87bf73df8b8406a09b8f3ce054bba5
Reddy Yattpu <reddy.yattpu@digital.hmrc.gov.uk>
2017-04-12 16:24:18 +0100

    SCRS-6186-BE-CREATE-API_FinancialCompliance

--- app/models/api/VatComplianceFinancial.scala
+++ app/models/api/VatComplianceFinancial.scala
@@ -22,7 +22,7 @@ case class VatComplianceFinancial(adviceOrConsultancyOnly: Boolean,
                                   actAsIntermediary: Boolean,
                                   chargeFees: Option[Boolean] = None,
                                   additionalNonSecuritiesWork: Option[Boolean] = None,
-                                  discretionaryinvestmentManagementServices: Option[Boolean] = None,
+                                  discretionaryiInvestmentManagementServices: Option[Boolean] = None,
                                   vehicleOrEquipmentLeasing: Option[Boolean] = None,
                                   investmentFundManagementServices: Option[Boolean] = None,
                                   manageFundsAdditonal: Option[Boolean] = None
